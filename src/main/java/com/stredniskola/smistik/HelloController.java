d4557c164e620ea1a890700e601ea220d7db9179
Matyáš Dospěl <it-matyasdospel@stredniskola.net>
2024-06-18 09:41:23 +0200

    Pridani slideru na kola

--- src/main/java/com/stredniskola/smistik/HelloController.java
+++ src/main/java/com/stredniskola/smistik/HelloController.java
@@ -2,18 +2,36 @@ package com.stredniskola.smistik;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Polygon auto;
     @FXML
     private ColorPicker barva;
+    @FXML
+    private Circle predni;
+    @FXML
+    private Circle zadni;
+    @FXML
+    private Slider slider;
 
+    boolean wracked = false;
 
     public void zmenBarvu(ActionEvent event) {
         auto.setFill(barva.getValue());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
